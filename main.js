69f3646cb0a2a0ef4ef8abe7ccec130af3393eeb
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-05-24 17:51:10 +0100

    Add js file

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const nav_menu = document.querySelector ('.nav-menu');
+const OpenBtn = document.querySelector('#open-menu-btn');
+const CloseBtn = document.querySelector ('#close-menu-btn');
+
+
+OpenBtn.addEventListener('click' , () => {
+
+    nav_menu.style.display ='flex';
+
+    CloseBtn.style.display = 'inline-block';
+    OpenBtn.style.display ='none';
+})
+
+const CloseNav = () => {
+    nav_menu.style.display='none';
+
+    CloseBtn.style.display ='none';
+    OpenBtn.style.display ='inline-block';
+}
+
+
+CloseBtn.addEventListener ('click' , CloseNav);
